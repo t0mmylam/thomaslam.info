69acb4d825520383743f0b84c2ec3a6534781473
Tommy Lam <tommylam@tommys-mbp.mynetworksettings.com>
2022-12-14 12:14:22 -0500

    adding more to about

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,48 +14,56 @@ import "./index.css";
 function Navbar() {
   return (
     <header>
-      <Logo />
-      <div className="nav">
-        <div className="nav-words">
-          <NavLink className="nav-links underline-hover-effect" to="/">
-            Home
-          </NavLink>
-          <NavLink className="nav-links underline-hover-effect" to="/about">
-            About
-          </NavLink>
-          <NavLink className="nav-links underline-hover-effect" to="/experience">
-            Experience
-          </NavLink>
-          <NavLink className="nav-links underline-hover-effect" to="/projects">
-            Projects
-          </NavLink>
+      <div className="menu">
+        <Logo />
+        <div className="nav">
+          <div className="nav-words">
+            <NavLink className="nav-links underline-hover-effect" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-links underline-hover-effect" to="/about">
+              About
+            </NavLink>
+            <NavLink
+              className="nav-links underline-hover-effect"
+              to="/experience"
+            >
+              Experience
+            </NavLink>
+            <NavLink
+              className="nav-links underline-hover-effect"
+              to="/projects"
+            >
+              Projects
+            </NavLink>
+          </div>
+          <div className="nav-icons">
+            <Link className="nav-links underline-hover-effect" to="/">
+              <FaHome />
+            </Link>
+            <Link className="nav-links underline-hover-effect" to="/">
+              <FaInfoCircle />
+            </Link>
+            <Link className="nav-links underline-hover-effect" to="/">
+              <FaBriefcase />
+            </Link>
+            <Link className="nav-links underline-hover-effect" to="/">
+              <FaProjectDiagram />
+            </Link>
+          </div>
+          <a
+            className="icons underline-hover-effect nav-links"
+            href="https://github.com/t0mmylam/"
+          >
+            <FaGithub />
+          </a>
+          <a
+            className="icons underline-hover-effect nav-links"
+            href="https://Navlinkedin.com/in/thslam/"
+          >
+            <FaLinkedin />
+          </a>
         </div>
-        <div className="nav-icons">
-          <Link className="nav-links underline-hover-effect" to="/">
-            <FaHome />
-          </Link>
-          <Link className="nav-links underline-hover-effect" to="/">
-            <FaInfoCircle />
-          </Link>
-          <Link className="nav-links underline-hover-effect" to="/">
-            <FaBriefcase />
-          </Link>
-          <Link className="nav-links underline-hover-effect" to="/">
-            <FaProjectDiagram />
-          </Link>
-        </div>
-        <a
-          className="icons underline-hover-effect nav-links"
-          href="https://github.com/t0mmylam/"
-        >
-          <FaGithub />
-        </a>
-        <a
-          className="icons underline-hover-effect nav-links"
-          href="https://Navlinkedin.com/in/thslam/"
-        >
-          <FaLinkedin />
-        </a>
       </div>
     </header>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
-function Home() {
+function About() {
   return (
-    <div className="home-wrapper">
+    <div className="wrapper">
       <h1>About Thomas Lam</h1>
       <p>
         I'm currently a first-year at the{" "}
@@ -17,11 +16,30 @@ function Home() {
         </a>{" "}
         studying Computer Science. I am passionate about programming because it
         gives me the ability to tackle intricate problems that make a difference
-        in people's lives. In the future, I hope to as a Software Engineer on
-        projects that have a meaningful impact on the world.
+        in people's lives. In the future, I hope to work as a Software Engineer
+        on projects that have a meaningful impact on the world.
+      </p>
+      <p>
+        Check out some of the <NavLink to="/projects">projects</NavLink> that
+        I've created and worked on!
+      </p>
+      <p>
+        Outside of work, I enjoy listening to a variety of{" "}
+        <a
+          href="https://open.spotify.com/user/lam.tommy?si=c740f4e06b1b4e25"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          music
+        </a>
+        . Some of my favorite genres include pop, rock, and indie. I also love
+        snowboarding and have had the opportunity to visit some amazing resorts
+        like Snowbird, Breckenridge, and Park City. At some point in the future,
+        I hope to fulfill my dreams of visiting a resort amidst the peaks
+        of the European Alps.
       </p>
     </div>
   );
 }
 
-export default Home;
+export default About;
