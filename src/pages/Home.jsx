8c1157973e82061f19d7d04f538ccc80aa8de967
Tommy Lam <tommylam@Tommys-MacBook-Pro.local>
2022-12-14 20:59:45 -0500

    saving

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Logo from "../components/Logo";
 import {
-  FaGithub,
-  FaLinkedin,
   FaInfoCircle,
-  FaBriefcase,
   FaProjectDiagram,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -16,8 +13,8 @@ function Home() {
       <h1 id="title">Thomas Lam</h1>
       <span className="sub">Student Developer</span>
       <div className="home-buttons">
-        <Link to='/about' className='button'><span id='button-content'>About</span></Link>
-        <Link to='/projects' className='button'><span id='button-content'>Projects</span></Link>
+        <Link to='/about' className='button'><FaInfoCircle />About</Link>
+        <Link to='/projects' className='button'><FaProjectDiagram />Projects</Link>
       </div>
     </div>
   );
