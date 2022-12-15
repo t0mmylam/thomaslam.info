efe70db9cdb7ff76ea6a026f0f19844a25a46136
Tommy Lam <tommylam@tommys-mbp.myfiosgateway.com>
2022-12-15 18:07:46 -0500

    active logo navbar

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './index.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./index.css";
 
 function Logo() {
-    return (
-        <Link to='/' id='logo-link'>
-        <span id='tllt'>
-            <span id='T0'>
-                <span id='T01'></span>
-                <span id='T02'></span>
-            </span>
-            <span id='L0'></span>
-            <span id='L1'></span>
-            <span id='T1'>
-            <span id='T11'></span>
-                <span id='T12'></span>
-            </span>
+  return (
+    <NavLink
+      className={({ isActive }) => (isActive ? "navActive" : "navInactive")}
+      to="/"
+      id="logo-link"
+    >
+      <span id="tllt">
+        <span id="T0">
+          <span id="T01"></span>
+          <span id="T02"></span>
         </span>
-        </Link>
-    )
+        <span id="L0"></span>
+        <span id="L1"></span>
+        <span id="T1">
+          <span id="T11"></span>
+          <span id="T12"></span>
+        </span>
+      </span>
+    </NavLink>
+  );
 }
 
 export default Logo;
